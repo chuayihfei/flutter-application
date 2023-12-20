a9cf1809ba0e0054291db37b66b8ea8b3df24d34
weilin <weilin.t@digipen.edu>
2023-12-21 03:05:27 +0800

    cross button for video, not facing line code

--- flutter_application_1/unity/unity/Assets/Script/VideoPlayCollision.cs
+++ flutter_application_1/unity/unity/Assets/Script/VideoPlayCollision.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VideoPlayCollision : MonoBehaviour
 {
@@ -9,9 +10,27 @@ public class VideoPlayCollision : MonoBehaviour
 
     private void OnTriggerEnter(Collider collider)
     {
+        //if collider detects collision with player
         if(collider.gameObject.name == "indicator")
         {
             videoObject.SetActive(true);
+
+            videoObject.GetComponent<VideoPlayer>().errorReceived += delegate (VideoPlayer videoplayer, string message)
+            {
+                Debug.LogWarning("[VideoPlayer] Play Video Error: " + message);
+            };
+        }
+    }
+
+    public void PausePlay()
+    {
+        if(videoObject.GetComponent<VideoPlayer>().isPlaying)
+        {
+            videoObject.GetComponent<VideoPlayer>().Pause();
+        }
+        else
+        {
+            videoObject.GetComponent<VideoPlayer>().Play();
         }
     }
 }
--- flutter_application_1/unity/unity/Assets/Script/getPathDist.cs
+++ flutter_application_1/unity/unity/Assets/Script/getPathDist.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using Unity.XR.CoreUtils;
 
 public class getDistance : MonoBehaviour
 {
@@ -12,13 +13,18 @@ public class getDistance : MonoBehaviour
     [SerializeField]
     private TMP_Text displayRemainingText;
 
+    [SerializeField]
+    private XROrigin arOrigin;
+
     private NavMeshPath path;
 
     // Update is called once per frame
     void Update()
     {
         path = navigationManager.CalculatedPath;
-        DisplayDistanceRemaining();
+        //DisplayDistanceRemaining();
+
+        getAngle();
     }
 
     private float GetPathDistance()
@@ -42,4 +48,26 @@ public class getDistance : MonoBehaviour
 
         displayRemainingText.text += " m";
     }
+
+    private void getAngle()
+    {
+        if (path.status != NavMeshPathStatus.PathInvalid)
+        {
+            float angle = Vector3.Angle(path.corners[1] - arOrigin.Camera.transform.position, arOrigin.Camera.transform.forward);
+
+            displayRemainingText.text = angle.ToString();
+            //Debug.Log(angle);
+
+            Debug.DrawLine(arOrigin.transform.position, path.corners[1], Color.blue);
+            Debug.DrawRay(arOrigin.transform.position, arOrigin.Camera.transform.forward, Color.red);
+
+            
+            if (angle > 40.0f)
+            {
+                //vibrate until looking at line
+                //Handheld.Vibrate();
+            }
+        }
+    }
+
 }
